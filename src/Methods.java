a95aee079c9daab4a416416bb16eabe56187f255
Oskar Polak <biuro@fivarto.pl>
2018-02-26 09:57:42 +0100

    Initial commit

---
+++ src/Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    public static void main(String[] args) {
+        Methods methods = new Methods();
+        System.out.println(methods.add(5,5));
+    }
+
+
+    public int add(int a, int b){
+        return a + b;
+    }
+}
